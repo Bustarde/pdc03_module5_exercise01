06503ab4fa39e68b08e8f2f30d018149307d3d38
Dell <Dell@DESKTOP-941T6I6>
2023-04-25 18:46:40 +0800

    Add project files.

---
+++ module5_exercise/module5_exercise/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module5_exercise
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Exercise01Picker(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new exercise01picker());
+        }
+    }
+}
---
+++ module5_exercise/module5_exercise/exercise01picker.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace module5_exercise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class exercise01picker : ContentPage
+    {
+        public exercise01picker()
+        {
+            InitializeComponent();
+        }
+    }
+}
